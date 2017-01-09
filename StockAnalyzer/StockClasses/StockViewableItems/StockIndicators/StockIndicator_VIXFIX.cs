13600aa41472a3ff41f1ad6fe8afae9ba489e789
dadelcarbo <david.carbonel@free.fr>
2017-01-09 17:02:24 +0100

    VIXFIX

---
+++ StockAnalyzer/StockClasses/StockViewableItems/StockIndicators/StockIndicator_VIXFIX.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using StockAnalyzer.StockMath;
+
+namespace StockAnalyzer.StockClasses.StockViewableItems.StockIndicators
+{
+    public class StockIndicator_VIXFIX : StockIndicatorBase
+    {
+        public StockIndicator_VIXFIX()
+        {
+        }
+        public override IndicatorDisplayTarget DisplayTarget
+        {
+            get { return IndicatorDisplayTarget.NonRangedIndicator; }
+        }
+
+        public override string Definition
+        {
+            get { return "VIXFIX(int Period)"; }
+        }
+        public override object[] ParameterDefaultValues
+        {
+            get { return new Object[] { 20 }; }
+        }
+        public override ParamRange[] ParameterRanges
+        {
+            get { return new ParamRange[] { new ParamRangeInt(1, 500) }; }
+        }
+        public override string[] ParameterNames
+        {
+            get { return new string[] { "Period" }; }
+        }
+
+        public override string[] SerieNames { get { return new string[] { "VIXFIX(" + this.Parameters[0].ToString() + ")" }; } }
+
+        public override System.Drawing.Pen[] SeriePens
+        {
+            get
+            {
+                if (seriePens == null)
+                {
+                    seriePens = new Pen[] { new Pen(Color.Blue) };
+                }
+                return seriePens;
+            }
+        }
+
+        public override void ApplyTo(StockSerie stockSerie)
+        {
+            int period = (int)this.parameters[0];
+            FloatSerie vixFix = new FloatSerie(stockSerie.Count, this.SerieNames[0]);
+            this.series[0] = vixFix;
+
+
+            FloatSerie closeSerie = stockSerie.GetSerie(StockDataType.CLOSE);
+            FloatSerie highSerie = stockSerie.GetSerie(StockDataType.HIGH);
+            FloatSerie lowSerie = stockSerie.GetSerie(StockDataType.LOW);
+
+            for (int i = 0; i < period; i++)
+            {
+                vixFix[i] = 1;
+            } 
+            for (int i = period; i < stockSerie.Count; i++)
+            {
+                float highest = highSerie.GetMax(i - period, i);
+                float low = lowSerie[i];
+                vixFix[i] = 100f* (highest - low) / highest;
+            }
+        }
+
+        static string[] eventNames = new string[] { };
+        public override string[] EventNames
+        {
+            get { return eventNames; }
+        }
+        static readonly bool[] isEvent = new bool[] { };
+        public override bool[] IsEvent
+        {
+            get { return isEvent; }
+        }
+    }
+}
