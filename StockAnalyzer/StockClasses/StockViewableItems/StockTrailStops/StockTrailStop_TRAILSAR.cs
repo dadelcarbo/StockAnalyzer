40f1fba226dae8ea3710ce4c75275c7bffa32dd9
dadelcarbo <david.carbonel@free.fr>
2019-05-14 11:21:42 +0200

    Remove useles code

--- StockAnalyzer/StockClasses/StockViewableItems/StockTrailStops/StockTrailStop_TRAILSAR.cs
+++ StockAnalyzer/StockClasses/StockViewableItems/StockTrailStops/StockTrailStop_TRAILSAR.cs
@@ -30,8 +30,6 @@ namespace StockAnalyzer.StockClasses.StockViewableItems.StockTrailStops
         {
             FloatSerie longStopSerie;
             FloatSerie shortStopSerie;
-            FloatSerie highSerie = stockSerie.GetSerie(StockDataType.HIGH);
-            FloatSerie lowSerie = stockSerie.GetSerie(StockDataType.LOW);
 
             float step = (float)this.parameters[0] / 100f;
 
