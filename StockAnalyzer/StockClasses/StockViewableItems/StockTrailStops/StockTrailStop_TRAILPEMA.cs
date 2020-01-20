adb886dda2e16a1b616d6f09401ec866045d3a31
dadelcarbo <david.carbonel@free.fr>
2020-01-20 10:27:55 +0100

    Add Trail PEMA and TRAIL SSAR

--- StockAnalyzer/StockClasses/StockViewableItems/StockTrailStops/StockTrailStop_TRAILPEMA.cs
+++ StockAnalyzer/StockClasses/StockViewableItems/StockTrailStops/StockTrailStop_TRAILPEMA.cs
@@ -4,10 +4,10 @@ using System.Drawing;
 
 namespace StockAnalyzer.StockClasses.StockViewableItems.StockTrailStops
 {
-    public class StockTrailStop_TRAILEMA : StockTrailStopBase
+    public class StockTrailStop_TRAILPEMA : StockTrailStopBase
     {
-        public override string Definition => base.Definition + Environment.NewLine + 
-            "Draw a trail stop that is calculated as a EMA starting from the previous extremum.";
+        public override string Definition => base.Definition + Environment.NewLine +
+            "Draw a trail stop from the previous extremum that is parallel to the EMA.";
 
         public override IndicatorDisplayTarget DisplayTarget
         {
@@ -27,14 +27,14 @@ namespace StockAnalyzer.StockClasses.StockViewableItems.StockTrailStops
         {
             get { return new ParamRange[] { new ParamRangeInt(1, 500), new ParamRangeInt(1, 500) }; }
         }
-        public override string[] SerieNames { get { return new string[] { "TRAILEMA.LS", "TRAILEMA.SS" }; } }
+        public override string[] SerieNames { get { return new string[] { "TRAILPEMA.LS", "TRAILPEMA.SS" }; } }
         
         public override void ApplyTo(StockSerie stockSerie)
         {
             FloatSerie longStopSerie;
             FloatSerie shortStopSerie;
 
-            stockSerie.CalculateEMATrailStop((int)this.Parameters[0], (int)this.Parameters[1], out longStopSerie, out shortStopSerie);
+            stockSerie.CalculatePEMATrailStop((int)this.Parameters[0], (int)this.Parameters[1], out longStopSerie, out shortStopSerie);
             this.Series[0] = longStopSerie;
             this.Series[1] = shortStopSerie;
 
