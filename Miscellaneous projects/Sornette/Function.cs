4c37f8ad62aeddc34d756dac2720c4de8d72c0f8
dadelcarbo <david.carbonel@free.fr>
2020-09-17 09:52:04 +0200

    Add Sornette project

---
+++ Miscellaneous projects/Sornette/Function.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sornette
+{
+    public delegate double Fnct(double x);
+    public class Function
+    {
+        public Value[] Values { get; private set; }
+
+        public string Name { get; private set; }
+
+        static public Function CreateTrigo(string name, double start, double end, int nbValues, Fnct f, double omega, double phi)
+        {
+            double step = (end - start) / (double)(nbValues - 1);
+            var function = new Function()
+            {
+                Values = new Value[nbValues],
+                Name = name
+            };
+
+            double x = start;
+            for (int i = 0; i < nbValues; i++)
+            {
+                function.Values[i] = new Value
+                {
+                    X = x,
+                    Y = f(omega * x + phi)
+                };
+                x += step;
+            }
+
+            return function;
+        }
+        static public Function CreateSornette(double start, double end, int nbValues, double Tc, double omega, double phi)
+        {
+            double step = (end - start) / (double)(nbValues - 1);
+            var function = new Function()
+            {
+                Values = new Value[nbValues],
+                Name = "Sornette"
+            };
+
+            double x = start;
+            for (int i = 0; i < nbValues; i++)
+            {
+                function.Values[i] = new Value
+                {
+                    X = x,
+                    Y = Math.Cos(omega * x + phi)
+                };
+                x += step;
+            }
+
+            return function;
+        }
+    }
+}
---
+++ Miscellaneous projects/Sornette/ViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sornette
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private double min;
+        public double Min
+        {
+            get { return min; }
+            set
+            {
+                if (value != min)
+                {
+                    min = value;
+                    this.OnPropertyChanged("Min");
+                    Calculate();
+                }
+            }
+        }
+
+        private double max;
+        public double Max
+        {
+            get { return max; }
+            set
+            {
+                if (value != max)
+                {
+                    max = value;
+                    this.OnPropertyChanged("Max");
+                    Calculate();
+                }
+            }
+        }
+
+        private double omega;
+
+        public double ω
+        {
+            get { return omega; }
+            set
+            {
+                if (value != omega)
+                {
+                    omega = value;
+                    this.OnPropertyChanged("ω");
+                    Calculate();
+                }
+            }
+        }
+
+        private double phi;
+
+        public double φ
+        {
+            get { return phi; }
+            set
+            {
+                if (value != phi)
+                {
+                    phi = value;
+                    this.OnPropertyChanged("φ");
+                    Calculate();
+                }
+            }
+        }
+
+
+        public ViewModel()
+        {
+            this.min = 0;
+            this.max = Math.PI * 2;
+            this.omega = 1;
+            this.phi = 0;
+            Calculate();
+        }
+
+        private void Calculate()
+        {
+            this.F1 = Function.CreateTrigo("Cos", min, max, 500, Math.Cos, ω, φ);
+            this.F2 = Function.CreateTrigo("Sin", min, max, 500, Math.Sin, ω, φ);
+
+            this.OnPropertyChanged("F1");
+            this.OnPropertyChanged("F2");
+        }
+
+        public Function F1 { get; set; }
+        public Function F2 { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
