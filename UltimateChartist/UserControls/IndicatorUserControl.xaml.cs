05abf5696d3b5dfb0c71eea8a183fef777feaeb8
dadelcarbo <david.carbonel@free.fr>
2020-12-23 08:35:41 +0100

    Update UltimateChartist

---
+++ UltimateChartist/UserControls/IndicatorUserControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UltimateChartist.UserControls
+{
+    /// <summary>
+    /// Interaction logic for IndicatorUserControl.xaml
+    /// </summary>
+    public partial class IndicatorUserControl : UserControl
+    {
+
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register("Header", typeof(string), typeof(IndicatorUserControl));
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        public IndicatorUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
