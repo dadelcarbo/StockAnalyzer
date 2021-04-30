9c9bd2bd15b87e28a58eab83c320937072977e3e
dadelcarbo <david.carbonel@free.fr>
2021-04-30 12:57:31 +0200

    Add Tweet App

---
+++ TweetApp/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TweetApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ViewModel vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            vm = this.Resources["ViewModel"] as ViewModel;
+        }
+
+        private void ConnectBtn_Click(object sender, RoutedEventArgs e)
+        {
+            vm.Connect();
+        }
+    }
+}
---
+++ TweetApp/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TweetApp
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        const string NOT_CONNECTED = "Not Connected";
+        const string CONNECTED = "Connected";
+        private string status = NOT_CONNECTED;
+        public string Status
+        {
+            get { return status; }
+            set { if (status != value) { status = value; NotifyPropertyChanged("Status"); } }
+        }
+
+        public void Connect()
+        {
+            this.Status = this.status == CONNECTED ? NOT_CONNECTED : CONNECTED;
+        }
+        #region INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
