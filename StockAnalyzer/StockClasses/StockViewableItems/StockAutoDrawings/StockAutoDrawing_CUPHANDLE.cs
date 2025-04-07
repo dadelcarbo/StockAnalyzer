f8ac1f72872e830d0966711340e3b0bc5d85a363
dadelcarbo <david.carbonel@free.fr>
2025-04-07 21:52:57 +0200

    Improve cup and handle

--- StockAnalyzer/StockClasses/StockViewableItems/StockAutoDrawings/StockAutoDrawing_CUPHANDLE.cs
+++ StockAnalyzer/StockClasses/StockViewableItems/StockAutoDrawings/StockAutoDrawing_CUPHANDLE.cs
@@ -11,38 +11,49 @@ namespace StockAnalyzer.StockClasses.StockViewableItems.StockAutoDrawings
 
         public override string Definition => "Detect Cup and Handle patterns";
 
-        public override string[] ParameterNames => new string[] { "Period", "Right HL" };
+        public override string[] ParameterNames => new string[] { "Period", "Right HL", "Last Only" };
 
-        public override Object[] ParameterDefaultValues => new Object[] { 3, true };
+        public override Object[] ParameterDefaultValues => new Object[] { 3, true, true };
 
-        public override ParamRange[] ParameterRanges => new ParamRange[] { new ParamRangeInt(2, 500), new ParamRangeBool() };
+        public override ParamRange[] ParameterRanges => new ParamRange[] { new ParamRangeInt(2, 500), new ParamRangeBool(), new ParamRangeBool() };
 
         public override string[] SerieNames => new string[] { };
-        public override Pen[] SeriePens
-        {
-            get
-            {
-                seriePens ??= new Pen[] { };
-                return seriePens;
-            }
-        }
+        public override Pen[] SeriePens => seriePens ??= new Pen[] { };
+
         public override void ApplyTo(StockSerie stockSerie)
         {
             var period = (int)this.parameters[0];
             var rightHigherLow = (bool)this.parameters[1];
+            var lastOnly = (bool)this.parameters[2];
 
             // Detecting events
             this.CreateEventSeries(stockSerie.Count);
             var brokenUpEvents = this.Events[Array.IndexOf(this.EventNames, "BrokenUp")];
 
             FloatSerie closeSerie = stockSerie.GetSerie(StockDataType.CLOSE);
-            for (int i = period * 2; i < stockSerie.Count; i++)
+            if (lastOnly)
             {
-                var cupHandle = closeSerie.DetectCupHandle(i, period, rightHigherLow);
-                if (cupHandle != null)
+                for (int i = stockSerie.Count - 1; i > period; i--)
                 {
-                    brokenUpEvents[i] = true;
-                    this.DrawingItems.Insert(0, cupHandle);
+                    var cupHandle = closeSerie.DetectCupHandle(i, period, rightHigherLow);
+                    if (cupHandle != null)
+                    {
+                        brokenUpEvents[i] = true;
+                        this.DrawingItems.Insert(0, cupHandle);
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = period * 2; i < stockSerie.Count; i++)
+                {
+                    var cupHandle = closeSerie.DetectCupHandle(i, period, rightHigherLow);
+                    if (cupHandle != null)
+                    {
+                        brokenUpEvents[i] = true;
+                        this.DrawingItems.Insert(0, cupHandle);
+                    }
                 }
             }
         }
