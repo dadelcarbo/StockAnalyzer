49b1922fe850c1018af12662b1f5d61f9ccb9584
dadelcarbo <david.carbonel@free.fr>
2025-05-28 20:28:59 +0200

    Telerik Update

--- StockAnalyzer_Backup_2025.05.22_11.26.51/Saxo.OpenAPI/TradingServices/OrderService/OrderResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Saxo.OpenAPI.TradingServices
-{
-    public class OrderResponse
-    {
-        public string OrderId { get; set; }
-        public OrderResponse[] Orders { get; set; }
-    }
-}
