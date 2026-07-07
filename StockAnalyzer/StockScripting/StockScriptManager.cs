43ac4e2be6e739e9ada3f3d32aec7adde858b51e
dadelcarbo <david.carbonel@free.fr>
2026-07-07 17:29:08 +0200

    Migrate StockFilter to DataSerie

--- StockAnalyzer/StockScripting/StockScriptManager.cs
+++ StockAnalyzer/StockScripting/StockScriptManager.cs
@@ -18,12 +18,9 @@ namespace StockAnalyzer.StockScripting
     public class StockScriptManager
     {
         private static readonly string filterClassTemplate = @"
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using StockAnalyzer.StockClasses;
 using StockAnalyzer.StockClasses.StockViewableItems;
+using StockAnalyzer.StockData;
 
 namespace StockAnalyzer.StockScripting
 {
--- StockAnalyzer/StockScripting/TestStockFilterImpl.cs
+++ StockAnalyzer/StockScripting/TestStockFilterImpl.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using StockAnalyzer.StockClasses;
-using StockAnalyzer.StockClasses.StockViewableItems;
+﻿using StockAnalyzer.StockClasses;
 using StockAnalyzer.StockData;
 
 namespace StockAnalyzer.StockScripting
@@ -12,7 +7,26 @@ namespace StockAnalyzer.StockScripting
     {
         protected override bool MatchFilter(DataSerie dataSerie, StockDailyValue lastBar)
         {
-            return true;
+            var ema175 = dataSerie.GetIndicator("EMA(175)").Series[0][dataSerie.LastIndex];
+            if (lastBar.CLOSE < ema175)
+                return false;
+
+            var c1 = lastBar.VARIATION > 0.04f;
+            var c2 = false;
+            var c3 = false;
+
+            if (c1)
+            {
+                var closeSerie = dataSerie.GetSerie(StockDataType.CLOSE);
+                var highest = closeSerie.GetHighestIn(dataSerie.LastIndex);
+                c2 = highest > 20;
+                if (c2)
+                {
+                    c3 = closeSerie.GetHighestIn(dataSerie.LastIndex - 1) > (highest + 1);
+                }
+            }
+
+            return c1 && c2 && c3;
         }
     }
 }
