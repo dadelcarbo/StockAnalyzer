2d334392e7d19ac418a0ca85537944dc5223fa02
dadelcarbo <david.carbonel@free.fr>
2026-09-07 22:16:23 +0200

    Fix T bug in alert dialog

--- StockAnalyzerApp/CustomControl/AlertDialog/StockAlertDialog/StockAlertManagerDlg.cs
+++ StockAnalyzerApp/CustomControl/AlertDialog/StockAlertDialog/StockAlertManagerDlg.cs
@@ -11,23 +11,12 @@ namespace StockAnalyzerApp.CustomControl.AlertDialog.StockAlertDialog
             InitializeComponent(viewModel);
             this.stockAlertManagerCtrl.DataContext = viewModel;
 
-            this.stockAlertManagerCtrl.KeyDown += Control_KeyDown;
-
             this.Shown += FormShown;
         }
 
         private void FormShown(object sender, EventArgs e)
         {
             TileWindows();
-        }
-
-        private void Control_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            if (e.Key == System.Windows.Input.Key.T)
-            {
-                TileWindows();
-                e.Handled = true; // Mark the event as handled
-            }
         }
 
         private void TileWindows()
